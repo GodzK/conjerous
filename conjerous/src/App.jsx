f07faacd35a9c714cdf25170138c5cff5904c0ad
GodzK <64467457+GodzK@users.noreply.github.com>
2024-01-02 17:49:32 +0700

    update file {no bug}
    go 50% of navbar setting

--- conjerous/src/App.jsx
+++ conjerous/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import {Article, Brand , CTA ,  } from './components'
+import {Article, Brand , CTA , Feature , Navbar ,  } from './components'
+import {Blog, Features , Footer, Header , Possibility , Whatconjer } from './containers'
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <div className ="gradient-bg">
+       <Navbar/>
+       <Header/>
+      </div>
+      <Brand/>
+      <Whatconjer/>
+      <Feature/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
--- conjerous/src/components/navbar/Navbar.jsx
+++ conjerous/src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import './navbar.css'
+import './Navbar.css';
+import { RiMenu3Line , RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="Navbar-nav">
+      <div className="Navbar-nav-link">
+        <div className="Navbar-nav-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="Navbar-link-container">
+         <p><a href ="#home">Home </a></p>
+         <p><a href ="#Whatconjer">What is Conjerous</a></p>
+         <p><a href ="#possibility">Open Ai </a></p>
+         <p><a href ="#features">Case study</a></p>
+         <p><a href ="#blog">library</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
