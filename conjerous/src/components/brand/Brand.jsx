0bac0523455ed94be3123af767daf4e4000e41d3
GodzK <64467457+GodzK@users.noreply.github.com>
2024-01-06 16:38:48 +0700

    brands componentS

--- conjerous/src/components/brand/Brand.jsx
+++ conjerous/src/components/brand/Brand.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './brand.css'
+import { mongodb,express, react, node} from  './import'
+
 function Brand() {
   return (
-    <div>Brand</div>
+    <div className="brand section-padding">
+      
+      <div>
+        <img src={mongodb} alt="mongodb" />
+      </div>
+      <div>
+        <img src={express} alt="express" />
+      </div>
+      <div>
+        <img src={react} alt="react" />
+      </div>
+      <div>
+        <img src={node} alt="react" />
+      </div>
+    </div>
   )
 }
 
---
+++ conjerous/src/components/brand/import.jsx
@@ -0,0 +1,11 @@
+
+import mongodb from '../../assets/MongoDB.png'
+import express from '../../assets/express.png'
+import react from '../../assets/Reactpng.png'
+import node from '../../assets/nodejs.png'
+export {
+    mongodb,
+    express,
+    react,
+    node,
+}
