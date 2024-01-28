a9d9739068425dd2063dee514adbe3e5f8f421e2
GodzK <64467457+GodzK@users.noreply.github.com>
2024-01-28 16:03:25 +0700

    new

--- notice/src/components/navbar/Navbar.jsx
+++ notice/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Navbar.css";
+import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 import logo from "../../assets/logo.svg";
